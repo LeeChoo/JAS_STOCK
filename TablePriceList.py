48bcb0ab53d2b703ad85eb71c4cfaed2a2eabc49
Vo Thi Truc Nga <ngavo2010@gmail.com>
2017-07-25 11:54:13 +0700

    Add Code

--- TablePriceList.py
+++ TablePriceList.py
@@ -1,1 +1,10 @@
 #Create table Pricelist in Mysql
+
+import pymysql.cursors
+connection = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd="", db='stock', charset="utf8", cursorclass=pymysql.cursors.DictCursor)
+cursor = connection.cursor()
+sql = """create table Pricelist(time datetime, ma_ck_a char(20) not null, TC_b int not null, tran_c int not null, san_d int not null, giamua3_e int, klmua3_f int, giamua2_g int, klmua2_h int, giamua1_i int, klmua1_j int, tl_k int, giakhop_l int, klkhop_m int, tongkl_n int, giaban1_o int, klban1_p int, giaban2_q int, klban2_r int, giaban3_s int, klban3_t int, tb int, ts int, u int, giacao_v int, giathap_w int, DTNNmua_x int, y int, z int)"""
+
+cursor.execute(sql)
+connection.commit()
+connection.close()
